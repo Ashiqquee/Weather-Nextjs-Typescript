852a830e00ae36f339e37ce2b0a660f134f6f8c1
Ashiqquee <ashiqqueee@gmail.com>
2023-08-20 21:18:20 +0530

    error on deployement fixed

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ const [details, setDetails] = useState({
   });
   const [location,setLocation]=useState("")
   const [error,setError]=useState("")
-  const url=`http://api.weatherapi.com/v1/forecast.json?key=96464713b01548d3b7e60320230208&q=${location}&days=7&aqi=yes&alerts=yes`
+  const url=`https://api.weatherapi.com/v1/forecast.json?key=96464713b01548d3b7e60320230208&q=${location}&days=7&aqi=yes&alerts=yes`
   const handleSearch= async(e:React.KeyboardEvent <HTMLInputElement>)=>{
  if(e.key==="Enter"){
   e.preventDefault()
